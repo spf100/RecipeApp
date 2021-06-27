d82a0882992fccd34722d29156d2826d792fdfa7
spf100 <shane.p.fordyce@gmail.com>
2021-06-27 01:27:23 -0400

    original push

---
+++ src/Controller/login.java
@@ -0,0 +1,12 @@
+package Controller;
+import Model.testUserList;
+import Model.user;
+public class login {
+
+    testUserList testUserList;
+    user currentuser;
+
+    public void login(){
+
+    }
+}
---
+++ src/Model/user.java
@@ -0,0 +1,21 @@
+package Model;
+
+public class user {
+    String username;
+    String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+}
