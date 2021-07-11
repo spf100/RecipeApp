a9e8bc2c89a8d480509ba7b63f3bfe4f04d2caa9
spf100 <shane.p.fordyce@gmail.com>
2021-07-10 21:42:57 -0400

    Created functions to allow user addition/deletion created functions to allow recipe addition/deletion created functions for checks on if recipe(by title) exits & if password is correct to user created function to see if user exists created function to return user data and recipe data #Todo Allow User to Move to Landing Page #Todo create Registration #Todo Fix/Create UI for Login and Registration

--- src/_Services/CSV.java
+++ src/_Services/CSV.java
@@ -1,4 +1,5 @@
 package _Services;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,9 @@ public class CSV {
     3 columns in User CSV
     Column order: ID, Username, Password
      */
+
+
+    //Return all data from CSV of choice
     public static List<String[]> returnData(Integer path) throws IOException {
         List<String[]> data = new ArrayList<>();
         String filepath;
@@ -22,7 +26,6 @@ public class CSV {
                 System.out.println("didnt make it");
                 return data;
         }
-
         BufferedReader csvReader = new BufferedReader(new FileReader(filepath));
         String row="";
         System.out.println("loop");
@@ -32,19 +35,72 @@ public class CSV {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            String[] rowdata = new String[0];
+            String[] rowdata;
             rowdata = row.split(",");
             System.out.println("lets add:");
             System.out.println(row);
             data.add(rowdata);
         }
         csvReader.close();
-
         return data;
     }
 
+    //writer for csv of choice
+    private static boolean WriteToCSV(Integer path, List<String[]> list){
+        String filepath;
+        switch(path){
+            case 1: filepath = "C:\\Users\\Gingy\\IdeaProjects\\RecipeApp\\src\\Data\\UserData.csv";
+                break;
+            case 2: filepath = "C:\\Users\\Gingy\\IdeaProjects\\RecipeApp\\src\\Data\\Recipes.csv";
+                break;
+            default:
+                System.out.println("didnt make it");
+                return false;
+        }
+        try {
+            PrintWriter writer = new PrintWriter(filepath);
+            for (String[] row : list) {
+                StringBuilder rowBuilder = new StringBuilder();
+                for (String n : row) {
+                    rowBuilder.append(n).append(',');
+                }
+                rowBuilder.deleteCharAt(rowBuilder.length() - 1);
+                rowBuilder.append("\n");
+                writer.write(String.valueOf(rowBuilder));
+            }
+            writer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    //overwrite entire csv
+    public static boolean OverwriteCSV(Integer path, List<String[]> list){
+        return WriteToCSV(path, list);
+    }
+
+    //append to CSV
+    public static boolean AddToCSV(Integer path, List<String[]> list){
+        List<String[]> oldData;
+        try {
+            oldData = returnData(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Could not find old data");
+            return false;
+        }
+        oldData.addAll(list);
+        return WriteToCSV(path, oldData);
+    }
+
+    /*
+    User Checks/Removes/Additions
+     */
+
     public boolean AddUser(String Username, String Password) throws IOException {
-        List<String[]> data = new ArrayList<>();
+        List<String[]> data;
         data = returnData(1);
         String[] element = data.get(data.size()-1);
         String id = element[0];
@@ -56,31 +112,26 @@ public class CSV {
         }
         String[] userData = {id, Username, Password};
         data.add(userData);
-        //return true if worked
-        return true;
+        return AddToCSV(1, data);
     }
 
-    public boolean RemoveUser(String Username, String Password) throws IOException{
-        List<String[]> data = new ArrayList<>();
+    //remove user from csv
+    public boolean RemoveUser(String Username) throws IOException{
+        List<String[]> data;
         data = returnData(1);
-        String[] element = data.get(data.size()-1);
-        String id = element[0];
-        String[] userData = {id, Username, Password};
         for (String[] checkData : data) {
             if (checkData[1].equals(Username)) {
-                data.remove(userData);
-                return true;
+                data.remove(checkData);
+                return OverwriteCSV(1, data);
             }
         }
         return false;
     }
 
+    //check if password is correct for user
     public static boolean CheckPassword(String Username, String Password) throws IOException {
-        List<String[]> data = new ArrayList<>();
+        List<String[]> data;
         data = returnData(1);
-        String[] element = data.get(data.size()-1);
-        String id = element[0];
-        String[] userData = {id, Username, Password};
         for (String[] checkData : data) {
             if (checkData[1].equals(Username)) {
                 return checkData[2].equals(Password);
@@ -90,7 +141,87 @@ public class CSV {
         return false;
     }
 
+    //return all user data
+    public static String[] returnUser(String Username) throws IOException {
+        List<String[]> data;
+        String[] nothing = {};
+        data = returnData(1);
+        for (String[] checkData : data) {
+            if (checkData[1].equals(Username)) {
+                return checkData;
+            }
+        }
+        return nothing;
+    }
 
+    //check if user exists
+    public static boolean findUser(String Username) throws IOException {
+        List<String[]> data;
+        data = returnData(1);
+        for (String[] checkData : data) {
+            if (checkData[1].equals(Username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /*
+    Recipe Checks/Removes/Additions
+     */
+
+    //check if recipe by title exists
+    public static boolean CheckRecipe(String Title) throws IOException {
+        List<String[]> data;
+        data = returnData(2);
+        for (String[] checkData : data) {
+            if (checkData[0].equals(Title)) {
+               return true;
+            }
+        }
+        //user doesnt exist
+        return false;
+    }
+
+    //return recipe information
+    public static String[] GetRecipe(String Title) throws IOException {
+        List<String[]> data;
+        String[] array = new String[0];
+        data = returnData(2);
+        for (String[] checkData : data) {
+            if (checkData[0].equals(Title)) {
+                return checkData;
+            }
+        }
+        //user doesnt exist
+        return array;
+    }
+
+    //remove recipe from csv
+    public static boolean RemoveRecipe(String Title) throws IOException {
+        List<String[]> data;
+        data = returnData(2);
+        for (String[] checkData : data) {
+            if (checkData[0].equals(Title)) {
+                data.remove(checkData);
+                return OverwriteCSV(2, data);
+            }
+        }
+        return false;
+    }
+
+    //add recipe to csv
+    public static boolean AddRecipe(String[] Recipe) throws IOException {
+        if(CheckRecipe(Recipe[0])){
+            //needs new title
+            return false;
+        }
+        List<String[]> data;
+        data = returnData(2);
+        data.add(Recipe);
+        return AddToCSV(2, data);
+    }
 
 }
 
